7d07a30cbb59b32608ddeae60a6b1aa3f390855e
Anand Babu <anands2007@gmail.com>
2019-02-05 00:59:26 +0530

    working code

--- src/main/scala/com/ryde/Consumer.scala
+++ src/main/scala/com/ryde/Consumer.scala
@@ -1,14 +1,19 @@
 package com.ryde
 
-import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import java.util
+import java.util.ArrayList
+
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession, functions}
+import org.apache.spark.sql._
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.{KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import com.ryde.SaveToCassandra
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StructType, StructField, StringType}
 
 object Consumer {
 
@@ -29,12 +34,14 @@ object Consumer {
     val sparkConf = new SparkConf().setMaster("local[2]")
                     .setAppName("StreamingConsumer")
                     .set("spark.cassandra.connection.host", "127.0.0.1")
+//                    .set("spark.io.compression.codec", "snappy");
 //                    .options("spark.driver.allowMultipleContexts", true)
 
     val spark= SparkSession.builder
       .appName("StructuredStream")
       .master("local[2]")
       .getOrCreate()
+
     val sc = spark.sparkContext
     val streamingContext = new StreamingContext(sc, Seconds(2))
 
@@ -49,16 +56,12 @@ object Consumer {
 
     messages.foreachRDD (
       rdd => {
-//        rdd.foreach(println)
-//        rdd.toDF().printSchema()
-//        rdd.toDF().show(2)
-        val df = spark.sqlContext.read.json(rdd.toDS())
-        df.show(5)
-        df.printSchema()
-        val busDF = df.select("name","business_id", "address")
+
+val df = spark.read.json(rdd.toDS())
+val savedf = df.select("business_id", "address", "name")
 
         val saveOb = new SaveToCassandra
-        saveOb.appendToCassandraTableDF(busDF)
+        saveOb.appendToCassandraTableDF(savedf)
 
       })
 
