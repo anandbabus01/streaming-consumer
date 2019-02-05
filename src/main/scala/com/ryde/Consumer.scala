49238b0862bd64cd634aa5b710b0abe3fe20b9ca
Anand Babu <anands2007@gmail.com>
2019-02-05 23:57:35 +0530

    multiple consumers

--- src/main/scala/com/ryde/Consumer.scala
+++ src/main/scala/com/ryde/Consumer.scala
@@ -29,22 +29,23 @@ object Consumer {
       "auto.offset.reset" -> "latest",
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
-    val topics = Array("sample")
 
     val sparkConf = new SparkConf().setMaster("local[2]")
-                    .setAppName("StreamingConsumer")
-                    .set("spark.cassandra.connection.host", "127.0.0.1")
-//                    .set("spark.io.compression.codec", "snappy");
-//                    .options("spark.driver.allowMultipleContexts", true)
+      .setAppName("StreamingConsumer")
+      .set("spark.cassandra.connection.host", "127.0.0.1")
+    //                    .set("spark.io.compression.codec", "snappy");
+    //                    .options("spark.driver.allowMultipleContexts", true)
 
-    val spark= SparkSession.builder
+    val spark = SparkSession.builder
       .appName("StructuredStream")
       .master("local[2]")
       .getOrCreate()
 
     val sc = spark.sparkContext
     val streamingContext = new StreamingContext(sc, Seconds(2))
+    val saveOb = new SaveToCassandra
 
+    val topics = Array("business")
     val dStream = KafkaUtils.createDirectStream[String, String](
       streamingContext,
       LocationStrategies.PreferConsistent,
@@ -54,15 +55,100 @@ object Consumer {
     val messages = dStream.map(ConsumerRecord => ConsumerRecord.value())
     import spark.implicits._
 
-    messages.foreachRDD (
+    messages.foreachRDD(
       rdd => {
+        val df = spark.read.json(rdd.toDS())
+        val saveDf = df.select("business_id", "name", "address","city","state","postal_code","stars" )
+        saveOb.appendToCassandraTableDF(saveDf,topics(0))
+      })
 
-val df = spark.read.json(rdd.toDS())
-val savedf = df.select("business_id", "address", "name")
+    val topic2 = Array("checkin") //"photo","review","tip","user")
+    val checkInStream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      LocationStrategies.PreferConsistent,
+      Subscribe[String, String](topic2, kafkaParams)
+    )
 
-        val saveOb = new SaveToCassandra
-        saveOb.appendToCassandraTableDF(savedf)
+    val checkInMessages = checkInStream.map(ConsumerRecord => ConsumerRecord.value())
 
+    checkInMessages.foreachRDD (
+      checkInRdd => {
+        val checkInDf = spark.read.json(checkInRdd.toDS())
+//        checkInDf.printSchema()
+        val saveCheckInDf = checkInDf.select("business_id","date")
+        saveOb.appendToCassandraTableDF(saveCheckInDf,topic2(0))
+      })
+
+    val topic3 = Array("photo") //"photo","review","tip","user")
+    val photoStream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      LocationStrategies.PreferConsistent,
+      Subscribe[String, String](topic3, kafkaParams)
+    )
+
+    val photoMessages = photoStream.map(ConsumerRecord => ConsumerRecord.value())
+    import spark.implicits._
+
+    photoMessages.foreachRDD (
+      photoRdd => {
+        val photoDf = spark.read.json(photoRdd.toDS())
+//        photoDf.printSchema()
+        val savephotoDf = photoDf.select("business_id","caption", "label","photo_id")
+        saveOb.appendToCassandraTableDF(savephotoDf,topic3(0))
+      })
+
+    val topic4 = Array("review") //"review","tip","user")
+    val reviewStream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      LocationStrategies.PreferConsistent,
+      Subscribe[String, String](topic4, kafkaParams)
+    )
+
+    val reviewMessages = reviewStream.map(ConsumerRecord => ConsumerRecord.value())
+    import spark.implicits._
+
+    reviewMessages.foreachRDD (
+      reviewRdd => {
+        val reviewDf = spark.read.json(reviewRdd.toDS())
+//        reviewDf.printSchema()
+        val saveReviewDf = reviewDf.select("business_id","caption", "label","photo_id")
+        saveOb.appendToCassandraTableDF(saveReviewDf,topic4(0))
+      })
+
+    val topic5 = Array("tip") //"tip","user")
+    val tipStream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      LocationStrategies.PreferConsistent,
+      Subscribe[String, String](topic5, kafkaParams)
+    )
+
+    val tipMessages = tipStream.map(ConsumerRecord => ConsumerRecord.value())
+    import spark.implicits._
+
+    tipMessages.foreachRDD (
+      tipRdd => {
+        val tipDf = spark.read.json(tipRdd.toDS())
+        tipDf.printSchema()
+        val saveTipDf = tipDf.select("business_id","compliment_count", "user_id","date")
+        saveOb.appendToCassandraTableDF(saveTipDf,topic5(0))
+      })
+
+    val topic6 = Array("tip") //"user")
+    val userStream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      LocationStrategies.PreferConsistent,
+      Subscribe[String, String](topic6, kafkaParams)
+    )
+
+    val userMessages = userStream.map(ConsumerRecord => ConsumerRecord.value())
+    import spark.implicits._
+
+    userMessages.foreachRDD (
+      userRdd => {
+        val userDf = spark.read.json(userRdd.toDS())
+        userDf.printSchema()
+        val saveUserDf = userDf.select("business_id","compliment_count", "user_id","date")
+        saveOb.appendToCassandraTableDF(saveUserDf,topic6(0))
       })
 
     streamingContext.start()
--- src/main/scala/com/ryde/SaveToCassandra.scala
+++ src/main/scala/com/ryde/SaveToCassandra.scala
@@ -8,10 +8,10 @@ import org.apache.spark.sql.{DataFrame, SaveMode}
 import scala.collection.mutable
 
 class SaveToCassandra {
-  def appendToCassandraTableDF(df : DataFrame)={
+  def appendToCassandraTableDF(df : DataFrame, tableName : String)={
     val options = new util.HashMap[String, String]
-    options.put("keyspace", "test".toLowerCase)
-    options.put("table", "details")
+    options.put("keyspace", "yelp_data".toLowerCase)
+    options.put("table", tableName)
 
     try {
       df.write.format("org.apache.spark.sql.cassandra").mode(SaveMode.Append).options(options).save()
