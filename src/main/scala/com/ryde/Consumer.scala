8abb3a7a9091a4d5ef9130d437028ad967bb2981
Anand Babu <anands2007@gmail.com>
2019-02-06 11:23:40 +0530

    one stream

--- src/main/scala/com/ryde/Consumer.scala
+++ src/main/scala/com/ryde/Consumer.scala
@@ -57,102 +57,18 @@ object Consumer {
 
     messages.foreachRDD(
       rdd => {
-        val df = spark.read.json(rdd.toDS())
-        df.show(5)
-        val saveDf = df.select("business_id", "name", "address","city","state","postal_code","stars" )
-        saveOb.appendToCassandraTableDF(saveDf,topics(0))
+        if ((rdd.isEmpty()) && rdd != null) {
+          print("No data received in stream\n")
+        }
+        else {
+          val df = spark.read.json(rdd.toDS())
+          df.show(5)
+          val saveDf = df.select("business_id", "name", "address", "city", "state", "postal_code", "stars")
+          saveOb.appendToCassandraTableDF(saveDf, topics(0))
+        }
       })
-
-    val topic2 = Array("checkin") //"photo","review","tip","user")
-    val checkInStream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
-      LocationStrategies.PreferConsistent,
-      Subscribe[String, String](topic2, kafkaParams)
-    )
-
-    val checkInMessages = checkInStream.map(ConsumerRecord => ConsumerRecord.value())
-
-    checkInMessages.foreachRDD (
-      checkInRdd => {
-        val checkInDf = spark.read.json(checkInRdd.toDS())
-//        checkInDf.printSchema()
-        val saveCheckInDf = checkInDf.select("business_id","date")
-        saveOb.appendToCassandraTableDF(saveCheckInDf,topic2(0))
-      })
-
-    val topic3 = Array("photo") //"photo","review","tip","user")
-    val photoStream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
-      LocationStrategies.PreferConsistent,
-      Subscribe[String, String](topic3, kafkaParams)
-    )
-
-    val photoMessages = photoStream.map(ConsumerRecord => ConsumerRecord.value())
-    import spark.implicits._
-
-    photoMessages.foreachRDD (
-      photoRdd => {
-        val photoDf = spark.read.json(photoRdd.toDS())
-//        photoDf.printSchema()
-        val savephotoDf = photoDf.select("business_id","caption", "label","photo_id")
-        saveOb.appendToCassandraTableDF(savephotoDf,topic3(0))
-      })
-
-    val topic4 = Array("review") //"review","tip","user")
-    val reviewStream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
-      LocationStrategies.PreferConsistent,
-      Subscribe[String, String](topic4, kafkaParams)
-    )
-
-    val reviewMessages = reviewStream.map(ConsumerRecord => ConsumerRecord.value())
-    import spark.implicits._
-
-    reviewMessages.foreachRDD (
-      reviewRdd => {
-        val reviewDf = spark.read.json(reviewRdd.toDS())
-//        reviewDf.printSchema()
-        val saveReviewDf = reviewDf.select("business_id","caption", "label","photo_id")
-        saveOb.appendToCassandraTableDF(saveReviewDf,topic4(0))
-      })
-
-    val topic5 = Array("tip") //"tip","user")
-    val tipStream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
-      LocationStrategies.PreferConsistent,
-      Subscribe[String, String](topic5, kafkaParams)
-    )
-
-    val tipMessages = tipStream.map(ConsumerRecord => ConsumerRecord.value())
-    import spark.implicits._
-
-    tipMessages.foreachRDD (
-      tipRdd => {
-        val tipDf = spark.read.json(tipRdd.toDS())
-        tipDf.printSchema()
-        val saveTipDf = tipDf.select("business_id","compliment_count", "user_id","date")
-        saveOb.appendToCassandraTableDF(saveTipDf,topic5(0))
-      })
-
-    val topic6 = Array("tip") //"user")
-    val userStream = KafkaUtils.createDirectStream[String, String](
-      streamingContext,
-      LocationStrategies.PreferConsistent,
-      Subscribe[String, String](topic6, kafkaParams)
-    )
-
-    val userMessages = userStream.map(ConsumerRecord => ConsumerRecord.value())
-    import spark.implicits._
-
-    userMessages.foreachRDD (
-      userRdd => {
-        val userDf = spark.read.json(userRdd.toDS())
-        userDf.printSchema()
-        val saveUserDf = userDf.select("business_id","compliment_count", "user_id","date")
-        saveOb.appendToCassandraTableDF(saveUserDf,topic6(0))
-      })
-
-    streamingContext.start()
+      streamingContext.start()
     streamingContext.awaitTermination()
   }
+
 }
